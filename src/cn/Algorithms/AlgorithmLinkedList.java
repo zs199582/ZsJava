342478e3b93fbec1c9adc7e57b60f2f600fb3b87
zs199582 <1181262579@qq.com>
2019-04-15 20:28:41 +0800

    第二次提交

--- src/cn/Algorithms/AlgorithmLinkedList.java
+++ src/cn/Algorithms/AlgorithmLinkedList.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class AlgorithmLinkedList {
     public void reverseLinkedList(ListNode node)
     {
-
+            System.out.println("aa");
     }
     /**
      * 给定一个带头结点的单链表，请将其逆序。head->7->6->5->4->3 变成 head->3->4->5->6->7
